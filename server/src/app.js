23d6980e74257eadd34b65c82dc3ccccb77330ec
Abhishek Kumar <abhishek@Abhisheks-MacBook-Air.local>
2026-08-08 20:53:03 +0530

    Make BASE_URL and CORS origin configurable via env vars for deployment

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials:true
 }));
 
